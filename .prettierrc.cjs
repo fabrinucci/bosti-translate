3ccd53c45a91cfd78b10e3dc9ca2eb966b9bc291
Fabrizio <fabrinucci99@gmail.com>
2023-07-12 16:00:45 -0300

    first commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  jsxSingleQuote: true,
+  singleQuote: true,
+  semi: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
